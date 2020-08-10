65276defe66c1c61ed3e1d84e207545a566722de
pranjal <pranjal1998jjn@gmail.com>
2020-08-10 15:44:43 +0530

    Updates

---
+++ precache-manifest.799e50d4c3ae8e499a18ab709832b80a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e95c604026b8b7b0969f298d9543496",
+    "url": "/doctor-appointment-app/index.html"
+  },
+  {
+    "revision": "a0351ef2f3729cbf966c",
+    "url": "/doctor-appointment-app/static/css/2.d34346ea.chunk.css"
+  },
+  {
+    "revision": "c83e830076f093beb5d1",
+    "url": "/doctor-appointment-app/static/css/main.c20f2454.chunk.css"
+  },
+  {
+    "revision": "a0351ef2f3729cbf966c",
+    "url": "/doctor-appointment-app/static/js/2.75ba366f.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/doctor-appointment-app/static/js/2.75ba366f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c83e830076f093beb5d1",
+    "url": "/doctor-appointment-app/static/js/main.ddda1ed0.chunk.js"
+  },
+  {
+    "revision": "e297ad3a92499d45b881",
+    "url": "/doctor-appointment-app/static/js/runtime-main.1be49a47.js"
+  },
+  {
+    "revision": "2f6b841cea950c98f0a38617682aa5d7",
+    "url": "/doctor-appointment-app/static/media/doctor.2f6b841c.jpg"
+  }
+]);
