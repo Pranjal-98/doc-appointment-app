b56eb97fd83c2f170da14335b6a4853fe5703bf9
Pranjal <pranjalc@geekyants.com>
2020-08-11 18:22:43 +0530

    Updates

---
+++ precache-manifest.329e19ab6a4efa26bf28605d4a7ebb83.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad36c3af5223d6dba365b9e1e8d8332a",
+    "url": "/doc-appointment-app/index.html"
+  },
+  {
+    "revision": "1819a6e48dbc6e628147",
+    "url": "/doc-appointment-app/static/css/2.d34346ea.chunk.css"
+  },
+  {
+    "revision": "5ad8814d1ce2be53710d",
+    "url": "/doc-appointment-app/static/css/main.631c1139.chunk.css"
+  },
+  {
+    "revision": "1819a6e48dbc6e628147",
+    "url": "/doc-appointment-app/static/js/2.a31813e1.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/doc-appointment-app/static/js/2.a31813e1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5ad8814d1ce2be53710d",
+    "url": "/doc-appointment-app/static/js/main.4d05fced.chunk.js"
+  },
+  {
+    "revision": "ee095c20854b2805344e",
+    "url": "/doc-appointment-app/static/js/runtime-main.68f99de2.js"
+  },
+  {
+    "revision": "2f6b841cea950c98f0a38617682aa5d7",
+    "url": "/doc-appointment-app/static/media/doctor.2f6b841c.jpg"
+  }
+]);
